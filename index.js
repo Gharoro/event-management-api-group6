7e81e57c39c9869037bc5f502d365b267ab2d428
Jerrycaffe <jerrycaffe@gmail.com>
2020-07-03 22:41:51 +0100

    merged conflicts

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import morgan from 'morgan';
-import passport from 'passport';
-import connect from './config/dbconnection';
 import auth from './routes/api/auth';
 import center from './routes/api/center';
 import passportConfig from './config/passport';
@@ -9,7 +7,6 @@ import customerProfile from './routes/api/customerProfile'
 import connect from './config/dbconnection';
 import custormer from './routes/api/authCustomer';
 import passport from 'passport';
-import customerPassport from './config/passport';
 
 
 // App initalization
@@ -29,23 +26,13 @@ customerPassport(passport);
 // Connecting to database
 connect();
 
-// Body parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// morgan
-app.use(morgan('tiny'));
-
-// Passport middleware
-app.use(passport.initialize());
-passportConfig(passport);
-
-app.use('/api/auth', auth);
-app.use('/api/center', center);
 
 // Routes
-// Customer Sign in route
+app.use('/api/auth', auth);
+app.use('/api/center', center);
 app.use('/auth/customer', custormer)
+
 
 app.get('/', (req, res) => {
   res.send('Event Management Application API - SGA Group 6');
