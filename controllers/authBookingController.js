aa28ed86e28dfff260dfedd7d90e4a433dd0e756
Jerrycaffe <jerrycaffe@gmail.com>
2020-07-09 16:33:43 +0100

    worked on customer cancel booked event hall

--- controllers/authBookingController.js
+++ controllers/authBookingController.js
@@ -1,4 +1,5 @@
 import models from "../models";
+import { check } from "express-validator";
 const makeBooking = async (req, res, next) => {
   const { event_date, from, to, purpose, additional_info } = req.body;
 
@@ -12,6 +13,26 @@ const makeBooking = async (req, res, next) => {
   const centerId = req.params.centerId;
 
   try {
+    const findCenter = await models.Centers.findOne({
+      where: { id: centerId }
+    });
+
+    if (!findCenter) {
+      return res
+        .status(404)
+        .json({
+          status: 404,
+          message: "cannot book an event hall that does not exist"
+        });
+    }
+
+    const centerNotAvailable = await models.Centers.update(
+      {
+        available: false
+      },
+      { where: { id: centerId } }
+    );
+
     const booking = await models.booking.create({
       event_date,
       from,
@@ -21,12 +42,16 @@ const makeBooking = async (req, res, next) => {
       additional_info,
       centerId
     });
-    if (booking) {
+
+    // set center availability to false
+
+    if (booking && centerNotAvailable) {
       return res.status(200).json({
         status: 200,
-        success: "Booking successfull"
+        success: "you have successfully book this Hall"
       });
     }
+
     return res.status(500).json({
       status: 500,
       message: "Unable to make booking at the moment"
@@ -37,4 +62,59 @@ const makeBooking = async (req, res, next) => {
   }
 };
 
-export { makeBooking };
+const cancelBooking = async (req, res, next) => {
+  const customerId = req.user.id;
+  const bookingId = req.params.bookingId;
+  const centerId = req.params.centerId;
+
+  //confirm if it is the same customer that made the booking
+  try {
+    const checkCustomer = await models.booking.findOne(
+      { attributes: ["customerId"] },
+      {
+        where: {
+          customerId
+        }
+      }
+    );
+
+    if (!checkCustomer) {
+      return res.status(401).json({
+        status: 401,
+        message: "Sorry you cannot cancel this booking"
+      });
+    }
+    // Set center available to true in center and
+    const centerAvalilable = await models.Centers.update(
+      { available: true },
+      { where: { id: centerId } }
+    );
+
+    // set booking status to cancel
+    const deleteBooking = await models.booking.update(
+      {
+        status: "cancel"
+      },
+      {
+        where: {
+          id: bookingId
+        }
+      }
+    );
+    if (deleteBooking && centerAvalilable) {
+      return res.status(200).json({
+        status: 200,
+        message: "This booking has been canceled"
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return next(error);
+  }
+  return res.status(200).json({
+    status: 500,
+    message: "You cannot cancel this booking at this time"
+  });
+};
+
+export { makeBooking, cancelBooking };
--- routes/api/authBooking.js
+++ routes/api/authBooking.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 const router = Router();
 
-import { makeBooking } from "../../controllers/authBookingController";
+import { makeBooking, cancelBooking } from "../../controllers/authBookingController";
 import {customerProtect} from "../../middleware/auth";
 
 router.post("/:centerId/booking", customerProtect, makeBooking);
+router.patch("/:centerId/:bookingId/cancelBooking", customerProtect, cancelBooking);
 
 export default router;
