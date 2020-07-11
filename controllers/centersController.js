25b186b42c5774e89bb1a155c77f615d8971eb5e
Gwachukwu <gwachukwuelebeke@gmail.com>
2020-07-11 23:48:52 +0100

    delete and update center

--- controllers/centersController.js
+++ controllers/centersController.js
@@ -4,7 +4,7 @@ const addCenter = async (req, res, next) => {
   if (req.user.role !== "admin") {
     return res.status(403).json({
       status: 403,
-      message: "You are not allowed to access this resource"
+      message: "You are not allowed to access this resource",
     });
   }
 
@@ -23,19 +23,19 @@ const addCenter = async (req, res, next) => {
   ) {
     return res.status(400).json({
       status: 400,
-      message: "Please fill all fields"
+      message: "Please fill all fields",
     });
   }
   if (image === undefined) {
     return res.status(400).json({
       status: 400,
-      message: "Please upload a picture of your event center"
+      message: "Please upload a picture of your event center",
     });
   }
   if (allowedTypes.indexOf(image.mimetype) === -1) {
     return res.status(400).json({
       status: 400,
-      message: "Please upload a jpg, jpeg or png file"
+      message: "Please upload a jpg, jpeg or png file",
     });
   }
   try {
@@ -47,20 +47,20 @@ const addCenter = async (req, res, next) => {
       facilities,
       location,
       price,
-      images: image.path
+      images: image.path,
     });
     if (createCenter) {
       // return a success message on completion
       return res.json({
         status: 200,
         message: "Center created successfully",
-        center: createCenter
+        center: createCenter,
       });
     }
     // return an error message on failure
     return res.status(500).json({
       status: 500,
-      message: "Unable to create center at the moment"
+      message: "Unable to create center at the moment",
     });
   } catch (error) {
     return next(error);
@@ -72,7 +72,7 @@ const viewAllCenters = async (req, res, next) => {
     const centers = await models.Centers.findAll();
     return res.status(200).json({
       status: 200,
-      centers
+      centers,
     });
   } catch (error) {
     console.log(error);
@@ -86,7 +86,7 @@ const viewOneCenter = async (req, res, next) => {
     const center = await models.Centers.findByPk(id);
     return res.status(200).json({
       status: 200,
-      center
+      center,
     });
   } catch (error) {
     console.log(error);
@@ -94,4 +94,90 @@ const viewOneCenter = async (req, res, next) => {
   }
 };
 
-export { addCenter, viewAllCenters, viewOneCenter };
+const deleteCenter = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    //find center by id
+    const center = await models.Centers.findByPk(id);
+    //delete
+    center.destroy();
+    return res.status(200).json({
+      status: 200,
+      message: "center successfully deleted",
+    });
+  } catch (error) {
+    console.log(error);
+    return next();
+  }
+};
+
+const updateCenter = async (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(403).json({
+      status: 403,
+      message: "You are not allowed to update this resource",
+    });
+  }
+  const id = req.params.id;
+  const { name, description, capacity, facilities, location, price } = req.body;
+  const manager_id = req.user.id;
+  const image = req.file;
+  const allowedTypes = ["image/png", "image/jpeg"];
+  // Validations
+  if (
+    !name ||
+    !description ||
+    !capacity ||
+    !facilities ||
+    !location ||
+    !price
+  ) {
+    return res.status(400).json({
+      status: 400,
+      message: "Please fill all fields",
+    });
+  }
+  if (image === undefined) {
+    return res.status(400).json({
+      status: 400,
+      message: "Please upload a picture of your event center",
+    });
+  }
+  if (allowedTypes.indexOf(image.mimetype) === -1) {
+    return res.status(400).json({
+      status: 400,
+      message: "Please upload a jpg, jpeg or png file",
+    });
+  }
+  try {
+    const updatedCenter = await models.Centers.update(
+      {
+        manager_id,
+        name,
+        description,
+        capacity,
+        facilities,
+        location,
+        price,
+        images: image.path,
+      },
+      { where: { id } }//where id = req.params.id
+    );
+    if (updatedCenter) {
+      // return a success message on completion
+      return res.json({
+        status: 200,
+        message: "Center updated successfully",
+        center: updatedCenter,
+      });
+    }
+    // return an error message on failure
+    return res.status(500).json({
+      status: 500,
+      message: "Unable to update center at the moment",
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+export { addCenter, viewAllCenters, viewOneCenter, deleteCenter , updateCenter};
--- routes/api/center.js
+++ routes/api/center.js
@@ -1,14 +1,25 @@
 import { Router } from "express";
 
-import { addCenter, viewOneCenter, viewAllCenters} from "../../controllers/centersController";
+import {
+  addCenter,
+  viewOneCenter,
+  viewAllCenters,
+  deleteCenter,
+  updateCenter,
+} from "../../controllers/centersController";
 import { adminProtect } from "../../middleware/auth";
 import parser from "../../config/cloudinaryConfig";
 
 const router = Router();
 
-
 router.post("/add_center", adminProtect, parser.single("image"), addCenter);
 router.get("/view_all_centers", viewAllCenters);
 router.get("/view_one_center/:id", viewOneCenter);
-
+router.delete("/delete_center/:id", adminProtect, deleteCenter);
+router.patch(
+  "/upate_center/:id",
+  adminProtect,
+  parser.single("image"),
+  updateCenter
+);
 export default router;
