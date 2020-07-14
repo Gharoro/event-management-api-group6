ee7e76bbb8e6792231d081c5411daa4d9621c03e
Gharoro <gharoropureheart@gmail.com>
2020-07-14 10:53:41 +0100

    prepare for deploy

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,11 +1,15 @@
 // Database connection function goes here
-import { Sequelize } from 'sequelize';
-import { config } from 'dotenv';
+import {
+  Sequelize
+} from 'sequelize';
+import {
+  config
+} from 'dotenv';
 
 config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: 'localhost',
+  host: process.env.DB_HOST,
   dialect: 'postgres',
   logging: true,
 });
