02e824d662db15ab5d12852bc52f9775dd91d471
Gharoro <gharoropureheart@gmail.com>
2020-08-15 20:50:46 +0100

    added customer view bookings

--- routes/api/customer.js
+++ routes/api/customer.js
@@ -3,7 +3,8 @@ import { Router } from "express";
 import {
   signup,
   signin,
-  customerProfile
+  customerProfile,
+  customerBookings,
 } from "../../controllers/customerController";
 import { customerProtect } from "../../middleware/auth";
 
@@ -12,5 +13,6 @@ const router = Router();
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.get("/profile", customerProtect, customerProfile);
+router.get("/bookings", customerProtect, customerBookings);
 
 export default router;
