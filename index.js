e7f0accb541241fc5ade327a583af1752785b437
Gharoro <gharoropureheart@gmail.com>
2020-08-16 08:28:34 +0100

    removed model associations

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 import express from "express";
 import morgan from "morgan";
-import cors from 'cors';
+import cors from "cors";
 
 import auth from "./routes/api/authAdmin";
 import customer from "./routes/api/customer";
 import center from "./routes/api/center";
 import booking from "./routes/api/booking";
-import {
-  connect
-} from "./config/dbconnection";
+import { connect, dropTables } from "./config/dbconnection";
 
 // App initalization
 const app = express();
@@ -19,11 +17,13 @@ app.use(cors());
 // Connecting to database
 connect();
 
+//dropTables();
+
 // middlewares
 app.use(express.json());
 app.use(
   express.urlencoded({
-    extended: true
+    extended: true,
   })
 );
 app.use(morgan("tiny"));
@@ -37,16 +37,18 @@ app.use("/api/booking", booking);
 app.get("/", (req, res) => {
   res.status(200).json({
     success: true,
-    message: 'Hello there! Welcome to Magnitude Event Manager.',
-    api_doc: 'https://documenter.getpostman.com/view/6511530/T17NbQMH?version=latest'
-  })
+    message: "Hello there! Welcome to Magnitude Event Manager.",
+    api_doc:
+      "https://documenter.getpostman.com/view/6511530/T17NbQMH?version=latest",
+  });
 });
-
 
 app.get("/api-docs", (res) => {
-  res.redirect(301, 'https://documenter.getpostman.com/view/6511530/T17NbQMH?version=latest');
+  res.redirect(
+    301,
+    "https://documenter.getpostman.com/view/6511530/T17NbQMH?version=latest"
+  );
 });
-
 
 const port = process.env.PORT || 900;
 
@@ -54,7 +56,6 @@ if (!module.parent) {
   app.listen(port, () => {
     console.log(`Server started on port ${port}`);
   });
-};
-
+}
 
 export default app;
